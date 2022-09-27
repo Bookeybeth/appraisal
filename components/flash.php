285aa07fccdd766d1fb61b6b5e6415a62a11d7ff
Bookeybeth <112193600+Bookeybeth@users.noreply.github.com>
2022-09-27 16:36:01 +0100

    updates

--- components/flash.php
+++
@@ -1,4 +0,0 @@
-<?php if(isset($_SESSION['flash'])): ?> 
-    <?php echo $_SESSION['flash']; $_SESSION['flash'] = null ?>
-<?php endif; ?>
-
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include('configuration/session.php');
-    unset_session();
-    header("Location: index.php");
-    $_SESSION['flash'] = flash('Logout successful', 'primary');
-    exit();
-?>
